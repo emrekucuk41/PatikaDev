71015eb7d1d5914b2ca05fcc6a28c851ce93cb2e
emrekucuk41 <emre10436@gmail.com>
2025-07-07 00:35:46 +0300

    Initial commit for Java101 projects

---
+++ NotOrtalamasiHesaplama.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+import javax.naming.event.NamingExceptionEvent;
+public class NotOrtalamasiHesaplama {
+    public static void main(String[] args){
+        int mat,fizik,kimya,turkce,tarih,muzik;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik Notunu Giriniz:");
+        mat = input.nextInt();
+
+        System.out.print("Fizik Notunuzu Giriniz:");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya Notunuzu Giriniz:");
+        kimya = input.nextInt();
+
+        System.out.print("Türkçe Notunuzu Giriniz:");
+        turkce = input.nextInt();
+
+        System.out.print("Tarih Notunuzu Giriniz:");
+        tarih = input.nextInt();
+
+        System.out.print("Müzik Notunuzu Giriniz:");
+        muzik = input.nextInt();
+
+        double ortalama = (mat+fizik+kimya+turkce+tarih+muzik)/6;
+
+        String sonuc = (ortalama >= 60) ? "Geçtiniz." : "Kaldınız.";
+        System.out.println(sonuc);
+
+
+    }
+}
